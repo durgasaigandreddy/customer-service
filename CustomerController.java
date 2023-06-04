b11fccd59f593db8d56e726798ee635ad935088c
durgasaigandreddy <126542736+durgasaigandreddy@users.noreply.github.com>
2023-06-04 14:01:20 +0530

    Add files via upload

---
+++ CustomerController.java
@@ -0,0 +1,31 @@
+package com.abc.customer.controller;
+
+import com.abc.customer.entity.Customer;
+import com.abc.customer.repository.CustomerRepository;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+    private CustomerRepository customerRepository;
+
+    public CustomerController(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @GetMapping()
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public Customer createCustomer(@ModelAttribute Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+
+}
